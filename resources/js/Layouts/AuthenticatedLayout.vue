9c56457d102389b426d9a3865fc272e7ad639126
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-08-09 23:24:49 +0100

    add nav links

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -17,7 +17,7 @@ const showingNavigationDropdown = ref(false);
                 </Link>
             </div>
             <nav class="mt-6">
-                <!-- <NavLink :href="route('categories.index')" :active="route().current('categories.index')">
+                <NavLink :href="route('categories.index')" :active="route().current('categories.index')">
                     Categories
                 </NavLink>
                 <NavLink :href="route('skill-levels.index')" :active="route().current('skill-levels.index')">
@@ -28,7 +28,7 @@ const showingNavigationDropdown = ref(false);
                 </NavLink>
                 <NavLink :href="route('users.index')" :active="route().current('users.index')">
                     Users
-                </NavLink> -->
+                </NavLink>
             </nav>
         </aside>
 
